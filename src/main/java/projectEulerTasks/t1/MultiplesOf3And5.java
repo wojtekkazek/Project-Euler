19836eb892b0589f4ce72c133dfc8b0e48405130
wojtekkazek <wojciech.kazek@gmail.com>
2021-04-15 13:32:17 +0200

    1

---
+++ src/main/java/projectEulerTasks/t1/MultiplesOf3And5.java
@@ -0,0 +1,40 @@
+package projectEulerTasks.t1;
+
+public class MultiplesOf3And5 {
+
+    public static void main(String[] args) {
+        int sumOfAllMultiplesOf3Or5Below1000 = calculateSumOfMultiplesOfTwoNumbersBelowNumber(3, 5, 1000);
+        System.out.println(sumOfAllMultiplesOf3Or5Below1000);
+    }
+
+    public static int calculateSumOfMultiplesOfTwoNumbersBelowNumber(int multiplier1, int multiplier2, int max) {
+        int sumOfMultipliesOfNumber1 = calculateSumOfMultiplesOfNumberBelowNumber(multiplier1, max);
+        int sumOfMultipliesOfNumber2 = calculateSumOfMultiplesOfNumberBelowNumber(multiplier2, max);
+        int leastCommonMultiplier = findLeastCommonMultiplierAbove(multiplier1, multiplier2, 0);
+        int sumOfMultipliesOfCommonMultiplier = calculateSumOfMultiplesOfNumberBelowNumber(leastCommonMultiplier, max);
+        return sumOfMultipliesOfNumber1 + sumOfMultipliesOfNumber2 - sumOfMultipliesOfCommonMultiplier;
+    }
+
+    public static int findLeastCommonMultiplierAbove (int number1, int number2, int min) {
+        int n = min + 1;
+        while (n % number1 != 0 || n % number2 != 0) {
+            n = findLeastCommonMultiplierAbove (number1, number2, n);
+        }
+        return n;
+    }
+
+    public static int calculateSumOfMultiplesOfNumberBelowNumber (int multiplyingNumber, int max) {
+        int maxMultiplication = findMaxMultiplicationBelow(multiplyingNumber, max);
+        double sum = (((double)multiplyingNumber + (double)maxMultiplication)/2 * maxMultiplication / multiplyingNumber);
+        return (int)sum;
+    }
+
+    public static int findMaxMultiplicationBelow (int multiplyingNumber, int max) {
+        int n = max - 1;
+        while (n % multiplyingNumber != 0) {
+            n = findMaxMultiplicationBelow(multiplyingNumber, n);
+        }
+        return n;
+    }
+
+}
