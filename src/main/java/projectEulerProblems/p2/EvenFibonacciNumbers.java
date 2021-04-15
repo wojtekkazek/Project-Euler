e87fd126d0ace3cb647ad05a7a8c2f2ad0498d21
wojtekkazek <wojciech.kazek@gmail.com>
2021-04-15 14:05:12 +0200

    2

--- src/main/java/projectEulerProblems/p2/EvenFibonacciNumbers.java
+++ src/main/java/projectEulerProblems/p2/EvenFibonacciNumbers.java
@@ -2,5 +2,26 @@ package projectEulerProblems.p2;
 
 public class EvenFibonacciNumbers {
 
+    public static void main(String[] args) {
+        int sumOfEvenFibonacciNumbersBelow4milion = findSumOfEvenFibonacciNumbersBelow(4000000);
+        System.out.println(sumOfEvenFibonacciNumbersBelow4milion);
+
+    }
+
+    public static int findSumOfEvenFibonacciNumbersBelow(int max) {
+        int i = 1;
+        int j = 2;
+        int n = i + j;
+        int sum = 2;
+        while (n < max) {
+            if(n % 2 == 0) {
+                sum += n;
+            }
+            i = j;
+            j = n;
+            n = i + j;
+        }
+        return sum;
+    }
 
 }
