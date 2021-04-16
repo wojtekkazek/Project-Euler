91484a34a9ba178d43befca7d020caece652d865
wojtekkazek <wojciech.kazek@gmail.com>
2021-04-16 12:33:49 +0200

    6

---
+++ src/main/java/projectEulerProblems/p6/SumSquareDifference.java
@@ -0,0 +1,32 @@
+package projectEulerProblems.p6;
+
+public class SumSquareDifference {
+    public static void main(String[] args) {
+        int differenceBetweenSquareOfSumAndSumOfSquaresOfNumbersBelow100 = calculateDifferenceBetweenSquareOfSumAndSumOfSquaresOfNumbersBelow(100);
+        System.out.println(differenceBetweenSquareOfSumAndSumOfSquaresOfNumbersBelow100);
+    }
+
+    public static int calculateDifferenceBetweenSquareOfSumAndSumOfSquaresOfNumbersBelow(int max) {
+        return calculateSquareOfSumOfNumbersBelow(max) - calculateSumOfSquaresOfNumbersBelow(max);
+    }
+
+    public static int calculateSquareOfSumOfNumbersBelow(int max) {
+        int sum = 0;
+        int n = 1;
+        while (n <= max) {
+            sum += n;
+            n += 1;
+        }
+        return sum * sum;
+    }
+
+    public static int calculateSumOfSquaresOfNumbersBelow(int max) {
+        int sum = 0;
+        int n = 1;
+        while (n <= max) {
+            sum += n * n;
+            n += 1;
+        }
+        return sum;
+    }
+}
