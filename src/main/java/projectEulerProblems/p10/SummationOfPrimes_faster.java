da82c1cd9b03aa711ff538696f7d5eaa83823cd7
wojtekkazek <wojciech.kazek@gmail.com>
2021-04-17 22:42:28 +0200

    10 - faster

--- src/main/java/projectEulerProblems/p10/SummationOfPrimes_faster.java
+++ src/main/java/projectEulerProblems/p10/SummationOfPrimes_faster.java
@@ -1,6 +1,6 @@
 package projectEulerProblems.p10;
 
-public class SummationOfPrimes {
+public class SummationOfPrimes_faster {
     public static void main(String[] args) {
         Long sumOfPrimesBelow2Milion = calculateSumOfPrimesBelow(2000000L);
         System.out.println(sumOfPrimesBelow2Milion);
@@ -8,20 +8,28 @@ public class SummationOfPrimes {
 
     public static Long calculateSumOfPrimesBelow (Long max) {
         Long sum = 2L;
+        int arraySize = (int)(max/2);
+        int n = 0;
+        Long primes[] = new Long[arraySize];
+        primes[n] = 2L;
         for (Long i = 3L; i < max; i += 2) {
-            if(checkIfPrimary(i)) {
+            if(checkIfPrimary(i, primes)) {
+                n++;
+                primes[n] = i;
                 sum += i;
             }
         }
         return sum;
     }
 
-    public static boolean checkIfPrimary (Long number) {
+    public static boolean checkIfPrimary (Long number, Long[] primesBelow) {
         double squareRoot = Math.pow(number, 0.5);
-        for (Long i = 3L; i <= squareRoot; i += 2) {
-            if (number % i == 0) {
+        int n = 0;
+        while (primesBelow[n] <= squareRoot) {
+            if (number % primesBelow[n] == 0) {
                 return false;
             }
+            n++;
         }
         return true;
     }
